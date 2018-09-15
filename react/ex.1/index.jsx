1152430f6e5951cd425ae3cb1ff9e32416c306d4
Pedro Lopes <pedrolopes.info@gmail.com>
2018-09-15 17:45:44 -0300

    react primeiro componente

---
+++ react/ex.1/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render( // renderiza um componete 
+    <h1>Olá React</h1>, 
+    document.getElementById('app') // destino
+) 
---
+++ react/ex/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Component from './componente'
+
+ReactDOM.render( // renderiza um componete 
+    <Component />, 
+    document.getElementById('app') // destino
+) 
